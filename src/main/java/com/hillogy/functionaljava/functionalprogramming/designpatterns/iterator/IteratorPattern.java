5aa1fef57b807ace7d65a5725493499cbc6af2c1
Placid Masvidal <pmasvidal@hillogy.com>
2021-04-08 11:07:17 +0200

    Iterator and Strategy patterns done

---
+++ src/main/java/com/hillogy/functionaljava/functionalprogramming/designpatterns/iterator/IteratorPattern.java
@@ -0,0 +1,13 @@
+package com.hillogy.functionaljava.functionalprogramming.designpatterns.iterator;
+
+public class IteratorPattern {
+
+    public static void main(String[] args) {
+
+        MyArrayList list = new MyArrayList(new Object[] {1,3,5,67,8});
+
+        list.forEach(System.out::println);
+
+    }
+
+}
---
+++ src/main/java/com/hillogy/functionaljava/functionalprogramming/designpatterns/iterator/MyArrayList.java
@@ -0,0 +1,19 @@
+package com.hillogy.functionaljava.functionalprogramming.designpatterns.iterator;
+
+import java.util.function.Consumer;
+
+public class MyArrayList {
+
+    Object[] elements = new Object[5];
+
+    public MyArrayList(Object[] elements) {
+        this.elements = elements;
+    }
+
+    public void forEach(Consumer<Object> action){
+        for(int i = 0; i < elements.length; i++){
+            action.accept(elements[i]);
+        }
+    }
+
+}
